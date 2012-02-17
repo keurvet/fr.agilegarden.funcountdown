dead4e2675db74e387761ea3cbf7215d19d220ee
Pierrick Thibault <keurvet2@cegetel.net>
2012-02-17 18:50:46 +0100

    Remplissage du readme

---
+++ funcountdown/src/funcountdown/readme.txt
@@ -0,0 +1,29 @@
+===================
+Fun CountDown Timer
+===================
+
+Installation
+============
+
+- Installer python 2.7 (ne fonctionne pas en 3.x) : 
+
+	- Sous Windows, télécharger et installer http://python.org/ftp/python/2.7.2/python-2.7.2.msi
+	- Sous ubuntu ::
+		
+		sudo apt-get install python
+	  
+	  (bien vérifier la version)
+	
+	
+- Installer pygame :
+	
+	- Sur Windows, télécharger et installer http://pygame.org/ftp/pygame-1.9.2a0.win32-py2.7.msi
+	- Sur ubuntu ::
+		
+		sudo apt-get install python-pygame
+
+
+Lancement
+=========
+
+Lancer funcountdown.py (double-click sur le fichier sous windows, idem sous Ubuntu après avoir rendu le fichier éxécutable).
---
+++ funcountdown/src/funcountdown/setUp.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+ 
+# source d'inspiration: http://wiki.wxpython.org/cx_freeze
+ 
+import sys, os
+from cx_Freeze import setup, Executable
+ 
+#############################################################################
+# preparation des options 
+path = sys.path.append(os.path.join("."))
+includes = []
+excludes = []
+packages = []
+ 
+options = {"path": path,
+           "includes": includes,
+           "excludes": excludes,
+           "packages": packages
+           }
+ 
+#############################################################################
+# preparation des cibles
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+ 
+cible_1 = Executable(
+    script = "funcountdown.py",
+    base = base,
+    compress = True,
+    icon = None,
+    )
+ 
+ 
+#############################################################################
+# creation du setup
+setup(
+    name = "funcd",
+    version = "1.0",
+    description = "An agile, simple and fun countdown timer to timebox professional or personal group activities like games or meetings",
+    author = "Pierrick Thibault",
+    options = {"build_exe": options},
+    executables = [cible_1]
+    )
